08c3131003f92101dc5782aab6fbe9318211dc03
mrlexcoder <mrlexcoder@gmail.com>
2025-03-02 12:10:23 +0530

    Initial commit for Techdulx 0

--- src/app/api/stripe/payment/route.ts
+++ src/app/api/stripe/payment/route.ts
@@ -5,7 +5,7 @@ import { isAuthorized } from "@/libs/isAuthorized";
 
 export async function POST(request: Request) {
 	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-		apiVersion: "2023-10-16",
+		apiVersion: "2025-02-24.acacia",
 	});
 	const data = await request.json();
 	const { isSubscribed, priceId, stripeCustomerId, userId } = data;
